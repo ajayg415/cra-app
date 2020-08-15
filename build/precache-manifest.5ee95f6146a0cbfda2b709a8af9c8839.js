19627f8caf135f2863de58a3f0c27241167991aa
ajayg415 <ajay.g415@gmail.com>
2020-08-15 11:22:25 +0530

    added gh-pages

---
+++ build/precache-manifest.5ee95f6146a0cbfda2b709a8af9c8839.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82c3efcc1b193a79dda713b52282870",
+    "url": "/cra-app/index.html"
+  },
+  {
+    "revision": "316e36e8d71817a4a526",
+    "url": "/cra-app/static/css/main.c847c747.chunk.css"
+  },
+  {
+    "revision": "752324d242d32c9b424a",
+    "url": "/cra-app/static/js/2.f8a985fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cra-app/static/js/2.f8a985fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "316e36e8d71817a4a526",
+    "url": "/cra-app/static/js/main.bdfc058d.chunk.js"
+  },
+  {
+    "revision": "cf251dd64b296e2fb462",
+    "url": "/cra-app/static/js/runtime-main.8c271f9b.js"
+  }
+]);
